2f59372923505aa242426a8b852ef875ddfe2308
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2023-01-11 20:51:35 -0300

    comit de teste

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,9 @@
   <nav class="bg-gray-300 py-4 drop-shadow-xl">
     <div class="flex justify-end">
       <div class="font-bold text-gray-500 mr-4 font-moon">Home</div>
-      <div class="font-mono font-bold text-gray-500 mr-4">Sobre Mim</div>
+      <div class="font-mono font-bold text-gray-500 mr-4 font-moon">
+        Sobre Mim
+      </div>
     </div>
   </nav>
 </template>
