d04d3e061ac6a381df9ff355f516414b0477ee14
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2023-01-11 20:50:15 -0300

    finalmente consegui adicionar uma fonte que funcione

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,9 @@
 <template>
-  <nav class="bg-gray-100 p-10">
-    <div class="font-mono font-bold text-gray-500">Home j</div>
+  <nav class="bg-gray-300 py-4 drop-shadow-xl">
+    <div class="flex justify-end">
+      <div class="font-bold text-gray-500 mr-4 font-moon">Home</div>
+      <div class="font-mono font-bold text-gray-500 mr-4">Sobre Mim</div>
+    </div>
   </nav>
 </template>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        nick: ["Nickainley-Normal"],
+        moon: ["Moon Dance"],
       },
     },
   },
