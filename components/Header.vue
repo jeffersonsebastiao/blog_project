e9d69080237f4fe4a50aa814820719dcb071b4e2
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2023-02-26 10:18:31 -0300

    header 'pronto'

--- components/Header.vue
+++ components/Header.vue
@@ -1,34 +1,34 @@
+<script lang="ts" setup>
+import { Icon } from "@iconify/vue";
+</script>
 <template>
-  <div class="py-4 bg-purple-300">
+  <div class="py-4 bg-gradient-to-tr from-purple-300 to-pink-200">
     <div class="flex justify-between max-w-7xl m-auto">
-      <div class="font-moon ml-4 font-bold text-5xl">Algodão Doce</div>
-      <div class="flex items-center">
-        <RouterLink
-          to="/"
-          class="font-bold hover:bg-blue-300 transition-all p-2 hover:rounded-md hover:p-2 mr-6 font-moon"
+      <div class="font-moon ml-4 font-bold text-zinc-800 text-5xl">
+        Algodão Doce
+      </div>
+      <div
+        class="flex text-zinc-800 font-bold font-moon items-center gap-6 text-2xl"
+      >
+        <RouterLink to="/" class="hover:scale-125 hover:transition-transform"
           >Home</RouterLink
         >
-        <RouterLink to="/build" class="font-bold hover:underline mr-4 font-moon"
+        <RouterLink
+          to="/build"
+          class="hover:scale-125 hover:transition-transform"
           >Sobre mim</RouterLink
         >
+        <RouterLink
+          to="/resenha"
+          class="hover:scale-125 hover:transition-transform"
+          >Reseha</RouterLink
+        >
+        <div class="flex ml-20 gap-2">
+          <Icon icon="ion:logo-instagram"></Icon>
+          <Icon icon="tabler:brand-twitter"> </Icon>
+        </div>
       </div>
     </div>
   </div>
-
-  <!-- <div class="bg-gray-300 py-4 items-center drop-shadow-xl">
-    <div class="flex text-3xl max-w-screen-sm text-center text-red-900">
-      <div class="font-moon ml-4 font-bold text-5xl">Algodão Doce</div>
-      <div>
-        <RouterLink
-          to="/"
-          class="font-bold hover:bg-blue-300 transition-all p-2 hover:rounded-md hover:p-2 mr-6 font-moon"
-          >Home</RouterLink
-        >
-        <RouterLink to="/build" class="font-bold hover:underline mr-4 font-moon"
-          >Sobre mim</RouterLink
-        >
-      </div>
-    </div>
-  </div> -->
 </template>
 <style scoped></style>
