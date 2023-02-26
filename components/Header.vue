acdaf1a077a6d82ef2a3fc2fb14abe5642e19009
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2023-02-25 21:09:23 -0300

    ajustes no topPage

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,34 @@
 <template>
-  <nav class="bg-gray-300 py-4 drop-shadow-xl">
-    <div class="flex justify-end">
-      <div class="font-bold text-gray-500 mr-4 font-moon">Home</div>
-      <div class="font-mono font-bold text-gray-500 mr-4 font-moon">
-        Sobre Mim
+  <div class="py-4 bg-purple-300">
+    <div class="flex justify-between max-w-7xl m-auto">
+      <div class="font-moon ml-4 font-bold text-5xl">Algodão Doce</div>
+      <div class="flex items-center">
+        <RouterLink
+          to="/"
+          class="font-bold hover:bg-blue-300 transition-all p-2 hover:rounded-md hover:p-2 mr-6 font-moon"
+          >Home</RouterLink
+        >
+        <RouterLink to="/build" class="font-bold hover:underline mr-4 font-moon"
+          >Sobre mim</RouterLink
+        >
       </div>
     </div>
-  </nav>
+  </div>
+
+  <!-- <div class="bg-gray-300 py-4 items-center drop-shadow-xl">
+    <div class="flex text-3xl max-w-screen-sm text-center text-red-900">
+      <div class="font-moon ml-4 font-bold text-5xl">Algodão Doce</div>
+      <div>
+        <RouterLink
+          to="/"
+          class="font-bold hover:bg-blue-300 transition-all p-2 hover:rounded-md hover:p-2 mr-6 font-moon"
+          >Home</RouterLink
+        >
+        <RouterLink to="/build" class="font-bold hover:underline mr-4 font-moon"
+          >Sobre mim</RouterLink
+        >
+      </div>
+    </div>
+  </div> -->
 </template>
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline"></h1>
+    <h1 class="text-3xl font-bold underline">OLA MUNDO</h1>
   </div>
 </template>
